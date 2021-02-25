ef0b71f0b02636b9508d505eeafb6c350511e591
Rares <rareschelariu79@gmail.com>
2021-02-25 14:57:47 +0200

    api for random joke and random no

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
 const http = require('http');
 const url = require('url');
-const randomNoAPI = require('./randomNoAPI')
+const APIrandomNo = require('./randomNoAPI')
+const APIjoke = require('./jokeAPI')
+
+const outputCallback = (response, data) => {
+    response.end(data)
+}
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain; charset=UTF-8'});
+
+    //res.end('ok bro')
     const purl = url.parse(req.url, true)
-    res.end('ok bro')
-    return
     switch (purl.pathname) {
-        // case "/random":
-        //     randomNoAPI.getRandomNo();
-        //     res.end("Heyy!")
-        //     break
+        case "/random":
+            res.writeHead(200, {'Content-Type': 'text/plain; charset=UTF-8'});
+            APIrandomNo.requestRandomNo(outputCallback, res)
+            break
         case "/joke":
-            res.end("Here u get the laughs!")
+            res.writeHead(200, {'Content-Type': 'application/json; charset=UTF-8'});
+            APIjoke.requestRandomJoke(outputCallback, res)
             break
         case "/meme":
+            res.writeHead(200, {'Content-Type': 'text/plain; charset=UTF-8'});
             res.end("Here u get the photo!")
             break
         default:
